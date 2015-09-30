682fc366d667eb62ef6e08527204c2dffee326b5
farhan0581 <farhan0581@gmail.com>
2015-10-01 00:03:14 +0530

    second

--- dash.php
+++ dash.php
@@ -1,29 +1,37 @@
 <?php 
 
+	function read_csv()
+	{
+		$handle=fopen('sample.csv', 'r');
+			if(!$handle)
+			{
+				echo "error";
+			}
+
+		$temp=fgetcsv($handle);//skip the heading...
+
+		while(!feof($handle))
+		{
+			$temp=fgetcsv($handle);
+			$dname=$temp[1];
+			$rest=$temp[2];
+			$query1="INSERT into dishes(dname,resturant) values('$dname','$rest')";
+			$status=mysqli_query($conn,$query1);
+			if($status)
+			{
+				echo "row inserted";
+			}
+		}
+
+	}
+
+	//call this to read file...
+	//read_csv();
 	$temp=array();
 	$flag=0;
 	$conn=mysqli_connect('localhost','root','','dashboard') or die('fatal!!');
-	$handle=fopen('sample.csv', 'r');
-	if(!$handle)
-	{
-		echo "error";
-	}
 
-	$temp=fgetcsv($handle);//skip the heading...
-
-	while(!feof($handle))
-	{
-		$temp=fgetcsv($handle);
-		$dname=$temp[1];
-		$rest=$temp[2];
-		// $query1="INSERT into dishes(dname,resturant) values('$dname','$rest')";
-		// $status=mysqli_query($conn,$query1);
-		// if($status)
-		// {
-		// 	echo "row inserted";
-		// }
-	}
-
+	
 	$query2="SELECT * from dishes where uploaded=0";
 
 	$result=mysqli_query($conn,$query2);
@@ -39,7 +47,7 @@
 <body>
 		<div class="container">
 		 	<h3>Dashboard</h3>
-		 	<form action="dash.php" method="get" enctype="multipart/formdata">
+		 	
 		 	<table class="table table-hover">
 		 		<tr>
 		 			<th>Dish</th>
@@ -49,43 +57,31 @@
 		 		</tr>
 		 	<?php 
 		 	
-		 		if($flag==0)
+		 		if($flag==0)//no use of this...
 		 			{	
 		 			while($rs=mysqli_fetch_array($result))
 		 			{
-		 	//&tname=<?php echo $_FILES['upload']['tmp_name'];
+		 	
 		 		 ?>
-
+		 		 <form action="dash_up.php?id=<?php echo $rs['did']; ?>" method="post" enctype="multipart/form-data">
 		 		<tr>
 		 			<td><?php echo $rs['dname']; ?></td>
 		 			<td><?php echo $rs['resturant']; ?></td>
-		 			<td><input  name="upload<?php echo $rs['did']; ?>" type="file" value="upload"></td>
-		 			<td><a class="btn btn-default" name="submit" href="dash.php?id=<?php echo $rs['did']; ?>">
-		 			Submit</a></td>
+		 			<td><input  name="upload" type="file" value="upload"></td>
+		 			<td><input class="btn btn-primary" type="submit" value="submit" name="submit"></td>
+		 			<input type="hidden" value="<?php echo $rs['did']; ?>" name="id">
 		 		</tr>
+		 		</form>
 		 		<?php 
 		 			}
 		 			$flag=1;
 		 		    }
 		 		 ?>
 		 	</table>
-		 	</form>
+		 	
 
 		</div>
 </body>
 </html>
 
-<?php 
-	
-	if(isset($_GET['id']))
-	{
-		$name="upload".$_GET['id'];
-
-		if(empty($_FILES[$name]))
-		{
-			echo "string";
-		}
-	}
-
- ?>
 
---
+++ dash_up.php
@@ -0,0 +1,37 @@
+<?php 
+
+		$conn=mysqli_connect('localhost','root','','dashboard') or die('fatal!!');
+
+		if(isset($_POST['submit']))
+		{
+			if(!empty($_FILES['upload']))
+			{
+				$id=$_POST['id'];
+				$name=$_FILES['upload']['name'];
+				$type=pathinfo($name, PATHINFO_EXTENSION);
+				$rename = $id.".".$type;
+				$tmp=$_FILES['upload']['tmp_name'];
+				$path='C:\xampp\htdocs\dashboard\images\\'.$rename;
+				if(move_uploaded_file($tmp, $path))
+				{
+					$query="UPDATE dishes set uploaded=1,dimage='$rename' where did=$id";
+					$res=mysqli_query($conn,$query);
+					if($res)
+					{
+						header("Location:dash.php");
+					}
+					else
+					{
+						echo "problem in storing in database";
+					}
+				}
+				else
+				{
+					echo "problem in image uploading";
+				}
+			}
+			
+
+
+		}
+ ?>
